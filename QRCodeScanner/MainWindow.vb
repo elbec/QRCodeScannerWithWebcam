4fadcb70941026fe4e57fc15e31bef1af08d6e08
elbec <45513949+elbec@users.noreply.github.com>
2019-02-03 17:14:09 +0100

    Add project files.

---
+++ QRCodeScanner/MainWindow.vb
@@ -0,0 +1,52 @@
+﻿Imports WebCam_Capture
+Imports MessagingToolkit.QRCode.Codec
+
+Public Class MainWindow
+    WithEvents MyWebcam As WebCamCapture
+    Dim Reader As QRCodeDecoder
+
+    Private Sub MyWebcam_ImageCaptured(source As Object, e As WebcamEventArgs) Handles MyWebcam.ImageCaptured
+        PictureBox1.Image = e.WebCamImage
+    End Sub
+
+    Private Sub StartWebcam()
+        Try
+            StopWebcam()
+            MyWebcam = New WebCamCapture
+            MyWebcam.Start(0)
+            MyWebcam.Start(0)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub StopWebcam()
+        Try
+            MyWebcam.Stop()
+            MyWebcam.Dispose()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        StartWebcam()
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        StopWebcam()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Scans the received image
+        Try
+            StopWebcam()
+            Reader = New QRCodeDecoder
+            TextBox1.Text = Reader.decode(New Data.QRCodeBitmapImage(PictureBox1.Image))
+            MsgBox("QR code is detected!")
+        Catch ex As Exception
+            StartWebcam()
+        End Try
+    End Sub
+End Class
